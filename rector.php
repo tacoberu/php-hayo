575f8ed74006cd1972cbb28a28d5ce7ac53ba35c
Martin Takáč <martin@takac.name>
2026-02-13 03:47:14 +0100

    add tools rector

---
+++ rector.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\DowngradeLevelSetList;
+use Rector\CodeQuality\Rector\ClassMethod\LocallyCalledStaticMethodToNonStaticRector;
+use Rector\Php71\Rector\List_\ListToArrayDestructRector;
+
+
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '/libs',
+		//~ __DIR__ . '/tests',
+	])
+	->withSets([
+		DowngradeLevelSetList::DOWN_TO_PHP_74,
+	])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        earlyReturn: true,
+    )
+    ->withSkip([
+        LocallyCalledStaticMethodToNonStaticRector::class, // mění static na non-static
+        ListToArrayDestructRector::class, // mění static na non-static
+        // SymplifyQuoteEscapeRector::class,  // Pokud nechceš měnit uvozovky
+        // RecastingRemovalRector::class,     // Odstraňuje zbytečné přetypování
+    ])
+    ->withPhpSets(
+        php74: true
+    )
+    ->withParallel()  // Výrazně zrychlí běh
+    ->withCache(__DIR__ . '/temp/rector')
+    ;
